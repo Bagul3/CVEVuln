b6ea2e3ab2785f5403418b3c57aea751d0ed8e48
Conor Shannon <conor_shannon@rapid7.com>
2017-06-26 17:33:09 +0100

    Code refactoring

--- SchedulerConsoleApp/Program.cs
+++ SchedulerConsoleApp/Program.cs
@@ -1,27 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using SchedulerManager.Mechanism;
+﻿using SchedulerManager.Mechanism;
 
 namespace SchedulerConsoleApp
 {
-    /// <summary>
-    /// Class which executes the program.
-    /// </summary>
-    class Program
+    public class Program
     {
-        /// <summary>
-        /// Method which instantiate the job manager and execute
-        /// all jobs.
-        /// </summary>
-        /// <param name="args">Arguments of execution. They are not
-        /// used here.</param>
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            // execute in ths project (asembly)
-            JobManager jobManager = new JobManager();
-            jobManager.ExecuteAllJobs();
+            new JobManager().ExecuteAllJobs();
         }
     }
 }
