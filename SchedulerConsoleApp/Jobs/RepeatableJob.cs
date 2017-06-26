80ec180bcf0914cd7bbeadc7544e721ebf10c2bf
Conor Shannon <conor_shannon@rapid7.com>
2017-06-26 17:23:46 +0100

    Code refactoring

--- SchedulerConsoleApp/Jobs/RepeatableJob.cs
+++ SchedulerConsoleApp/Jobs/RepeatableJob.cs
@@ -1,55 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using SchedulerManager.Mechanism;
 
 namespace SchedulerConsoleApp.Jobs
 {
-    /// <summary>
-    /// A simple repeatable Job.
-    /// </summary>
-    class RepeatableJob : Job
+    public class RepeatableJob : Job
     {
-        /// <summary>
-        /// Counter used to count the number of times this job has been
-        /// executed.
-        /// </summary>
-        private int counter = 0;
+        private int counter;
         
-        /// <summary>
-        /// Get the Job Name, which reflects the class name.
-        /// </summary>
-        /// <returns>The class Name.</returns>
         public override string GetName()
         {
             return this.GetType().Name;
         }
 
-        /// <summary>
-        /// Execute the Job itself. Just print a message.
-        /// </summary>
         public override void DoJob()
         {
-            System.Console.WriteLine(String.Format("This is the execution number \"{0}\" of the Job \"{1}\".", counter.ToString(), this.GetName()));
-            counter++;
+            Console.WriteLine($"This is the execution number \"{this.counter.ToString()}\" of the Job \"{this.GetName()}\".");
+            this.counter++;
         }
 
-        /// <summary>
-        /// Determines this job is repeatable.
-        /// </summary>
-        /// <returns>Returns true because this job is repeatable.</returns>
         public override bool IsRepeatable()
         {
             return true;
         }
 
-        /// <summary>
-        /// Determines that this job is to be executed again after
-        /// 1 sec.
-        /// </summary>
-        /// <returns>1 sec, which is the interval this job is to be
-        /// executed repeatadly.</returns>
+        public override string GetEndpoint()
+        {
+            return this.GetType().Name;
+        }
+
         public override int GetRepetitionIntervalTime()
         {
             return 1000;
